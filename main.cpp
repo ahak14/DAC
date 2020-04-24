a6a4981e37608c3f6fbe54de22fb1707e6ec827a
amirhossein <ah.abbasi.ah@gmail.com>
2020-04-24 19:51:50 +0430

    complete initial program

--- main.cpp
+++ main.cpp
@@ -15,11 +15,15 @@ int next_town_for_shortest_path_in_specific_days[MAX_TOWNS][MAX_TOWNS][MAX_TOWNS
 long long int meeting_cost_in_specific_town_days[MAX_TOWNS][MAX_TOWNS][MAX_TOWNS][MAX_TOWNS];
 // family in town(v) and family in town(u) go to town(z) in i days
 
+int families_town[MAX_TOWNS];
+bool meeting[MAX_TOWNS][MAX_TOWNS]; // i meet j in town(i)
+int meeting_number = 0;
 
 void get_inputs() {
     cin >> towns_number >> roads_number;
     for (int i = 1; i <= towns_number; i++) {
         cin >> residency_cost[i];
+        families_town[i] = i;
     }
     for (int i = 1; i <= towns_number; i++) {
         for (int j = 1; j <= towns_number; j++) {
@@ -109,8 +113,73 @@ void preprocess_meeting_cost_in_specific_town_days() {
     }
 }
 
+pair<pair<int, int>, pair<int, int> > find_min_meeting_cost_in_specific_town_days(const bool fixed_meeting[MAX_TOWNS]) {
+    long long int min_cost = INT32_MAX;
+    pair<pair<int, int>, pair<int, int> > best; // <i, j>, z -> i and j going to z
+    for (int day = 0; day <= towns_number; day++) {
+        for (int i = 1; i <= towns_number; i++) {
+            for (int j = 1; j <= towns_number; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (!fixed_meeting[i] && !fixed_meeting[j] && (!meeting[i][j] || !meeting[j][i])) {
+                    long long int temp1 = meeting_cost_in_specific_town_days[families_town[i]][families_town[j]][i][day];
+                    long long int temp2 = meeting_cost_in_specific_town_days[families_town[i]][families_town[j]][j][day];
+                    if (temp1 != -1 && temp1 < min_cost && !meeting[i][j]) {
+                        min_cost = temp1;
+                        best = {{i, j}, {i, day}};
+                    }
+                    if (temp2 != -1 && temp2 < min_cost && !meeting[j][i]) {
+                        min_cost = temp2;
+                        best = {{i, j}, {j, day}};
+                    }
+                }
+            }
+        }
+    }
+    return best;
+}
+
+vector<pair<pair<int, int>, pair<int, int> > > start() {
+    int day = 0;
+    vector<pair<pair<int, int>, pair<int, int> > > result;
+    int max_meeting_number = towns_number * (towns_number - 1);
+    while (meeting_number != max_meeting_number) {
+        day++;
+        int meetings_plan_number = towns_number / 2;
+        bool fixed_meeting[MAX_TOWNS];
+        fill(fixed_meeting, fixed_meeting + MAX_TOWNS, false);
+        for (int i = 0; i < meetings_plan_number; i++) {
+            pair<pair<int, int>, pair<int, int> > plan = find_min_meeting_cost_in_specific_town_days(fixed_meeting);
+            fixed_meeting[plan.first.first] = true;
+            fixed_meeting[plan.first.second] = true;
+            int new_town1 = next_town_for_shortest_path_in_specific_days[families_town[plan.first.first]][plan.second.first][plan.second.second];
+            int new_town2 = next_town_for_shortest_path_in_specific_days[families_town[plan.first.second]][plan.second.first][plan.second.second];
+            if (new_town1 != families_town[plan.first.first])
+                result.push_back({{1, day}, {plan.first.first, new_town1}});
+            if (new_town2 != families_town[plan.first.second])
+                result.push_back({{1, day}, {plan.first.second, new_town2}});
+            families_town[plan.first.first] = new_town1;
+            families_town[plan.first.second] = new_town2;
+            if (plan.second.second == 1) {
+                int host = plan.first.first == plan.second.first ? plan.first.first : plan.first.second;
+                int company = plan.first.first == host ? plan.first.second : plan.first.first;
+                result.push_back({{2, day}, {company, host}});
+                meeting[host][company] = true;
+                meeting_number++;
+            }
+        }
+    }
+    return result;
+}
+
 int main() {
     get_inputs();
     preprocess_shortest_path_cost_in_specific_days();
     preprocess_meeting_cost_in_specific_town_days();
+    vector<pair<pair<int, int>, pair<int, int> > > plans = start();
+    cout << plans.size() << endl;
+    for (auto & plan : plans) {
+        cout << plan.first.first << " " << plan.first.second << " " << plan.second.first << " " << plan.second.second << endl;
+    }
 }
